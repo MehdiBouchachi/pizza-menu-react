b398bec886f184d90bdaea5d835596ecefebf89b
MehdiBouchachi <mehdibch.dev@gmail.com>
2024-03-27 02:13:31 +0100

    Update package

--- src/index.js
+++ src/index.js
@@ -45,7 +45,6 @@ const pizzaData = [
     photoName: "pizzas/prosciutto.jpg",
     soldOut: false,
   },
-  
 ];
 
 function App() {
@@ -59,15 +58,19 @@ function App() {
 }
 
 function Header() {
+  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+
   return (
     <header className="header">
-      <h1>Fast Pizza Co.</h1>
+      <h1 style={style}>Fast React Pizza Co.</h1>
     </header>
   );
 }
 
 function Menu() {
   const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
 
   return (
@@ -75,51 +78,57 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <React.Fragment>
+        <>
           <p>
             Authentic Italian cuisine. 6 creative dishes to choose from. All
-            from our stone oven, all organic, all delicious
+            from our stone oven, all organic, all delicious.
           </p>
+
           <ul className="pizzas">
-            {pizzaData.map((pizza) => (
+            {pizzas.map((pizza) => (
               <Pizza pizzaObj={pizza} key={pizza.name} />
             ))}
           </ul>
-        </React.Fragment>
+        </>
       ) : (
-        <p>We're still working on our menu. Please back later :) </p>
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
 
-      {/*
-      <Pizza
+      {/* <Pizza
         name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, mushrooms, and onion"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
         price={10}
       />
-
       <Pizza
         name="Pizza Funghi"
-        ingredients="Tomato, mushrooms, and onion"
+        ingredients="Tomato, mushrooms"
         price={12}
         photoName="pizzas/funghi.jpg"
-      />*/}
+      /> */}
     </main>
   );
 }
 
 function Pizza({ pizzaObj }) {
-  //if (pizzaObj.soldOut) return null;
+  console.log(pizzaObj);
+
+  // if (pizzaObj.soldOut) return null;
 
   return (
     <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
-
         <p>{pizzaObj.ingredients}</p>
 
-        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price + " $"}</span>
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -127,39 +136,50 @@ function Pizza({ pizzaObj }) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
-  const closeHour = 21;
+  const openHour = 12;
+  const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-
   console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closed");
+
+  // if (!isOpen) return <p>CLOSED</p>;
+
   return (
     <footer className="footer">
       {isOpen ? (
-        <Open closeHour={closeHour} openHour={openHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
-          We're happy to welcome you between {openHour}:00 and {closeHour}:00{" "}
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
   );
+
+  // return React.createElement("footer", null, "We're currently open!");
 }
 
-function Open({ closeHour, openHour }) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
       <p>
-        We'r Open from {openHour}:00 until {closeHour}:00 Pm. Come visit us or
-        order online
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
       </p>
       <button className="btn">Order</button>
     </div>
   );
 }
 
+// React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// React before 18
+// ReactDOM.render(<App />, document.getElementById("root"));
